ef73242d27bf0a4d005da6bf73b5856cfaf10604
sakuramilk <c.sakuramilk@gmail.com>
2013-02-04 00:12:03 +0900

    Added I/O shedulers

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -2919,7 +2919,6 @@ static void changed_ioprio(struct io_context *ioc, struct cfq_io_context *cic)
 static void cfq_ioc_set_ioprio(struct io_context *ioc)
 {
 	call_for_each_cic(ioc, changed_ioprio);
-	ioc->ioprio_changed = 0;
 }
 
 static void cfq_init_cfqq(struct cfq_data *cfqd, struct cfq_queue *cfqq,
@@ -3211,8 +3210,13 @@ retry:
 		goto err_free;
 
 out:
-	smp_read_barrier_depends();
-	if (unlikely(ioc->ioprio_changed))
+	/*
+	 * test_and_clear_bit() implies a memory barrier, paired with
+	 * the wmb() in fs/ioprio.c, so the value seen for ioprio is the
+	 * new one.
+	 */
+	if (unlikely(test_and_clear_bit(IOC_CFQ_IOPRIO_CHANGED,
+					ioc->ioprio_changed)))
 		cfq_ioc_set_ioprio(ioc);
 
 #ifdef CONFIG_CFQ_GROUP_IOSCHED
@@ -3653,10 +3657,10 @@ static inline int __cfq_may_queue(struct cfq_queue *cfqq)
 		cfq_mark_cfqq_must_alloc_slice(cfqq);
 		return ELV_MQUEUE_MUST;
 	}
-	// Add I/O improvement code from U1
-       if (cfq_class_rt(cfqq)) {
-               return ELV_MQUEUE_MUST;
-       }    
+
+	/* if realtime class, return 'must queue' to improve I/O */
+	if (cfq_class_rt(cfqq))
+		return ELV_MQUEUE_MUST;
 
 	return ELV_MQUEUE_MAY;
 }
--- block/genhd.c
+++ block/genhd.c
@@ -1131,11 +1131,11 @@ static int disk_uevent(struct device *dev, struct kobj_uevent_env *env)
 		cnt++;
 	disk_part_iter_exit(&piter);
 	add_uevent_var(env, "NPARTS=%u", cnt);
-#ifdef CONFIG_USB_HOST_NOTIFY
+#ifdef CONFIG_USB_STORAGE_DETECT
 	if (disk->interfaces == GENHD_IF_USB) {
 		add_uevent_var(env, "MEDIAPRST=%d", disk->media_present);
-		printk(KERN_INFO "%s %d, disk->disk_name=%s, disk->media_present=%d, cnt=%d\n",
-				__func__, __LINE__, disk->disk_name, disk->media_present, cnt);
+		printk(KERN_INFO "%s %d, disk->media_present=%d, cnt=%d\n",
+				__func__, __LINE__, disk->media_present, cnt);
 	}
 #endif
 	return 0;
@@ -1619,7 +1619,7 @@ static void disk_events_workfn(struct work_struct *work)
 	unsigned long intv;
 	int nr_events = 0, i;
 
-#ifdef CONFIG_USB_HOST_NOTIFY
+#ifdef CONFIG_USB_STORAGE_DETECT
 	if (disk->interfaces != GENHD_IF_USB)
 		/* check events */
 		events = disk->fops->check_events(disk, clearing);
@@ -1646,7 +1646,7 @@ static void disk_events_workfn(struct work_struct *work)
 	for (i = 0; i < ARRAY_SIZE(disk_uevents); i++)
 		if (events & disk->events & (1 << i))
 			envp[nr_events++] = disk_uevents[i];
-#ifdef CONFIG_USB_HOST_NOTIFY
+#ifdef CONFIG_USB_STORAGE_DETECT
 		if (disk->interfaces != GENHD_IF_USB) {
 			if (nr_events)
 				kobject_uevent_env(&disk_to_dev(disk)->kobj,
