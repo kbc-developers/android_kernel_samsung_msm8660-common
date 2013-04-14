644c586f9b7f6c06a34b0f1b44062530221aa77b
Talustus <talustus@dream-irc.de>
2013-04-14 14:20:18 +0200

    mmc: increase read timeouts for bad SDHC cards

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -352,7 +352,7 @@ void mmc_set_data_timeout(struct mmc_data *data, const struct mmc_card *card)
 			 */
 			limit_us = 800000;
 		else
-			limit_us = 100000;
+			limit_us = 300000;
 
 		/*
 		 * SDHC cards always use these fixed values.
