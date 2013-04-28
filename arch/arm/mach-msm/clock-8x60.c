8c48e0021723e1769288978d7f27869f98586afe
HomuHomu <ecoco666@gmail.com>
2013-04-28 13:44:57 +0900

    Added JPN Model(SC03D,SC05D),build script and release-tools
    Conflicts:
    
    	arch/arm/configs/cyanogenmod_quincydcm_defconfig
    	arch/arm/mach-msm/Kconfig
    	arch/arm/mach-msm/board-msm8x60_q1.c
    	arch/arm/mach-msm/gpiomux-8x60_q1.c
    	arch/arm/mach-msm/qdsp6v2/board-msm8x60-audio.c
    	drivers/input/keyboard/Kconfig
    	drivers/input/keyboard/cypress-touchkey.c
    	drivers/input/touchscreen/qt602240_ts.c
    	drivers/media/Makefile
    	drivers/media/nmi326/nmi326.c
    	drivers/media/nmi326/nmi326.h

--- arch/arm/mach-msm/clock-8x60.c
+++ arch/arm/mach-msm/clock-8x60.c
@@ -2629,7 +2629,8 @@ static struct clk_freq_tbl clk_tbl_pixel_mdp[] = {
 	F_PIXEL_MDP( 24000000, pll8, 1,	  1,   16),
 #endif
 	F_PIXEL_MDP( 25600000, pll8, 3,   1,    5),
-#if defined (CONFIG_KOR_MODEL_SHV_E110S) || defined (CONFIG_USA_MODEL_SGH_I727) || defined (CONFIG_USA_MODEL_SGH_T989)	
+#if defined (CONFIG_KOR_MODEL_SHV_E110S) || defined (CONFIG_USA_MODEL_SGH_I727) \
+	|| defined (CONFIG_USA_MODEL_SGH_T989) || defined (CONFIG_JPN_MODEL_SC_03D)
 	F_PIXEL_MDP( 27400000, pll8, 1,	  1,   14), // for TDMB	
 #endif	
 	F_PIXEL_MDP( 42667000, pll8, 1,   1,    9),
