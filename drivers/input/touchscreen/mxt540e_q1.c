6ec8721eb5a51896d22b4de1ef0ca3cddd16ad53
HomuHomu <ecoco666@gmail.com>
2013-05-11 01:51:02 +0900

    SC05D:gcc-4.7 build::fix compile error

--- drivers/input/touchscreen/mxt540e_q1.c
+++ drivers/input/touchscreen/mxt540e_q1.c
@@ -626,7 +626,7 @@ static int __devinit mxt540e_init_touch_driver(struct mxt540e_data *data)
 {
 	struct object_t *object_table;
 	u32 read_crc = 0;
-	u32 calc_crc;
+	u32 calc_crc = 0;
 	u16 crc_address;
 	u16 dummy;
 	int i, j;
