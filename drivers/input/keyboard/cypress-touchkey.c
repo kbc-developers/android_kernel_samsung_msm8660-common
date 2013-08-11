628d86ca715acd41d4444a0717f843e1e8a0d241
blackzigong <blackzigong@gmail.com>
2013-08-11 09:26:15 -0700

    fix cypress-touchkey.c

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -823,12 +823,6 @@ static void sec_touchkey_early_resume(struct early_suspend *h)
 		gpio_request(GPIO_TOUCHKEY_SDA, "TKEY_SDA");
 		gpio_direction_input(GPIO_TOUCHKEY_SDA);
 		}
-#elif defined(CONFIG_JPN_MODEL_SC_05D)
-		tkey_vdd_enable(1);
-		gpio_request(GPIO_TOUCHKEY_SCL, "TKEY_SCL");
-		gpio_direction_input(GPIO_TOUCHKEY_SCL);
-		gpio_request(GPIO_TOUCHKEY_SDA, "TKEY_SDA");
-		gpio_direction_input(GPIO_TOUCHKEY_SDA);
 #endif // defined (CONFIG_USA_MODEL_SGH_T989)||defined (CONFIG_USA_MODEL_SGH_I727)
 	init_hw();
 
@@ -1420,7 +1414,7 @@ static ssize_t touchkey_menu_show(struct device *dev, struct device_attribute *a
     mutex_lock(&touchkey_driver->mutex);
     ret = i2c_touchkey_read(KEYCODE_REG, data, 18);
 
-    #if defined(CONFIG_KOR_MODEL_SHV_E160L) || defined(CONFIG_JPN_MODEL_SC_05D)
+    #if defined(CONFIG_KOR_MODEL_SHV_E160L)||defined(CONFIG_JPN_MODEL_SC_05D)
     printk("[TKEY] %s data[12] =%d,data[13] = %d\n",__func__,data[12],data[13]);
     menu_sensitivity = ((0x00FF&data[12])<<8)|data[13];
     #else
@@ -1706,7 +1700,7 @@ static ssize_t touch_sensitivity_control(struct device *dev, struct device_attri
 
 #if defined (CONFIG_USA_MODEL_SGH_T989) || defined(CONFIG_USA_MODEL_SGH_I727) || defined(CONFIG_USA_MODEL_SGH_I717) \
 || defined (CONFIG_KOR_MODEL_SHV_E110S)|| defined(CONFIG_KOR_MODEL_SHV_E160L) || defined(CONFIG_CAN_MODEL_SGH_I757M)\
-|| defined(CONFIG_USA_MODEL_SGH_I757) || defined(CONFIG_USA_MODEL_SGH_T769) || defined(CONFIG_USA_MODEL_SGH_I577) || defined(CONFIG_CAN_MODEL_SGH_I577R) || defined(CONFIG_JPN_MODEL_SC_03D) || defined(CONFIG_JPN_MODEL_SC_05D)
+|| defined(CONFIG_USA_MODEL_SGH_I757) || defined (CONFIG_USA_MODEL_SGH_T769) || defined(CONFIG_USA_MODEL_SGH_I577) || defined(CONFIG_CAN_MODEL_SGH_I577R) || defined(CONFIG_JPN_MODEL_SC_05D)
 static ssize_t touch_recommend_read(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	char data[3] = { 0, };
@@ -1772,7 +1766,7 @@ static ssize_t touch_recommend_read(struct device *dev, struct device_attribute 
 	printk("touch_recommend_read 0x%x\n", data[1]);
 	return count;
 }
-#endif
+#else
 static ssize_t set_touchkey_firm_version_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	/*TO DO IT */
@@ -1786,6 +1780,7 @@ static ssize_t set_touchkey_firm_version_show(struct device *dev, struct device_
 #endif
 	return count;
 }
+#endif
 
 #if defined(CONFIG_JPN_MODEL_SC_05D)
 static ssize_t set_touchkey_firm_version_show(struct device *dev, struct device_attribute *attr, char *buf)
@@ -2199,6 +2194,7 @@ static int __init touchkey_init(void)
 	}
 #elif defined (CONFIG_JPN_MODEL_SC_05D)
 	irq_set_irq_type(IRQ_TOUCHKEY_INT, IRQ_TYPE_EDGE_FALLING);
+
 #else
 		irq_set_irq_type(IRQ_TOUCHKEY_INT, IRQ_TYPE_LEVEL_LOW);
 #endif
@@ -2215,7 +2211,7 @@ static int __init touchkey_init(void)
         return 0;
 #endif
 
-//	printk("[TKEY] %s F/W version: 0x%x, Module version:0x%x, HW_REV: 0x%x\n", __FUNCTION__, data[1], data[2], get_hw_rev());
+	printk("[TKEY] %s F/W version: 0x%x, Module version:0x%x, HW_REV: 0x%x\n", __FUNCTION__, data[1], data[2], get_hw_rev());
 	touch_version = data[1];
 	retry = 3;
 
@@ -2524,7 +2520,7 @@ printk("%s : update SHV_E120L_WXGA tkey...\n",__func__);
 		   printk("%s : Touch Key version is latest. E110S %d!!!...\n",__func__, get_hw_rev());
 		}*/
 #elif defined (CONFIG_JPN_MODEL_SC_03D)
-/*		if (get_hw_rev() ==0x01 ){
+		if (get_hw_rev() ==0x01 ){
 			firm_version = 0x02;
 			if ((data[1] != 0x02) || (data[2] != 0x00))
 			{
@@ -2613,7 +2609,7 @@ printk("%s : update SHV_E120L_WXGA tkey...\n",__func__);
 				get_touchkey_firmware(data);
 				printk("%s change to F/W version: 0x%x, Module version:0x%x\n", __FUNCTION__, data[1], data[2]);
 			}
-		} */
+		}
 #endif
 	ret = i2c_add_driver(&touchkey_i2c_driver);
 
