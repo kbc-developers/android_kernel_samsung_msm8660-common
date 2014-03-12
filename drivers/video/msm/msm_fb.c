6755655c04c3f1146074886f4721178dfbbcafd7
flar2 <asegaert@gmail.com>
2014-03-11 20:22:25 -0400

    msm_fb: Squashed Add Backlight Dimmer
    Change-Id: I72045f12651b25635a4fd1bb7b7b09329259232b
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    Backlight dimmer option
    
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    backlight_dimmer: a bit less dim and disable by default
    
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    adjustable brightness
    
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    fix typos
    
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    disable by default
    
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    Remove useless include
    
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -62,6 +62,9 @@
 #undef CONFIG_HAS_EARLYSUSPEND
 #endif
 
+int backlight_dimmer = 0;
+module_param(backlight_dimmer, int, 0755);
+
 static unsigned char *fbram;
 static unsigned char *fbram_phys;
 static int fbram_size;
@@ -200,11 +203,21 @@ static void msm_fb_set_bl_brightness(struct led_classdev *led_cdev,
 		bl_lvl = 0;
 	else if (value >= MAX_BACKLIGHT_BRIGHTNESS)
 		bl_lvl = mfd->panel_info.bl_max;
-	else
+	else if (backlight_dimmer > 0) {
+		if (value <= backlight_dimmer) {
+			bl_lvl = 1;
+		} else {
+			bl_lvl = (mfd->panel_info.bl_min + ((value - 1) * 2 *
+				(mfd->panel_info.bl_max - mfd->panel_info.bl_min) +
+				MAX_BACKLIGHT_BRIGHTNESS - 1) /
+				(MAX_BACKLIGHT_BRIGHTNESS - 1) / 2) - backlight_dimmer;
+		}
+	} else {
 		bl_lvl = mfd->panel_info.bl_min + ((value - 1) * 2 *
 			(mfd->panel_info.bl_max - mfd->panel_info.bl_min) +
 			MAX_BACKLIGHT_BRIGHTNESS - 1) /
 			(MAX_BACKLIGHT_BRIGHTNESS - 1) / 2;
+	}
 
         down(&mfd->sem);
 	msm_fb_set_backlight(mfd, bl_lvl);
