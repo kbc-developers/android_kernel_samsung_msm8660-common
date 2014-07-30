4e54e99ab6d231e8e558cf364795e869d6822bfb
Nick Reuter <nreuter85@gmail.com>
2014-07-29 23:31:28 -0500

    Revert "msm8660-common: use MAX_FREQ_LIMIT from cpufreq.h"
    This reverts commit d656e8146aebb5af4c43cd9e50923c50bf7f2486.

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -884,7 +884,11 @@ static unsigned int __init select_freq_plan(void)
 		speed_bin = (pte_efuse >> 4) & 0xF;
 
 	if (speed_bin == 0x1) {
-		max_khz = MAX_FREQ_LIMIT;
+#ifdef CONFIG_USA_MODEL_SGH_I577
+		max_khz = 1242000;
+#else
+		max_khz = 1512000;
+#endif
 		pvs = (pte_efuse >> 10) & 0x7;
 		if (pvs == 0x7)
 			pvs = (pte_efuse >> 13) & 0x7;
