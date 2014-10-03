73eee928992c8da012d92df4cab6c295c2c0fb78
Emmanuel Utomi <emmanuelutomi@gmail.com>
2014-10-03 18:28:41 -0500

    cypress-touchkey: Support Sweep2Sleep and cleanup implementations
    Works just like sweep2wake, but buttons should be swept from right to left, as oppose to left to right. s2w_sensitive also may apply to this.
    
    There is also a new 3 second button sequence timeout
    
    Conflicts:
    	drivers/input/keyboard/cypress-touchkey.c
    	flashable/tools/updatewild.sh
    
    Conflicts:
    	drivers/input/keyboard/cypress-touchkey.c
    	drivers/input/keyboard/s2w-switch.c
    	include/linux/s2w-switch.h
    
    Change-Id: I06f0fdf515030811ebe36f648ab33406de5cd2ae

--- include/linux/s2w-switch.h
+++ include/linux/s2w-switch.h
@@ -15,6 +15,8 @@
 */
 
 extern int s2w_switch;
+extern int s2s_switch;
+extern int s2w_sensitive;
 
 /* The global /sys/android_touch/ kobject for people to chain off of */
 extern struct kobject *android_touch_kobj;
