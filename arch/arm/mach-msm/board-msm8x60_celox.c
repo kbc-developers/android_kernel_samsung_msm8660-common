01c6587f0cb63d5e489a68c2bfe6349eb1839eab
Paul Keith <javelinanddart@gmail.com>
2015-10-05 19:57:01 -0500

    uberdemand: Don't Hardcode Second Phase Frequency
    Change-Id: I45109b9e15b5fea65ad1c6e9bb5a9624a7e9e607
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- arch/arm/mach-msm/board-msm8x60_celox.c
+++ arch/arm/mach-msm/board-msm8x60_celox.c
@@ -16590,7 +16590,7 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 	set_two_phase_freq(1134000);
 #endif
 #ifdef CONFIG_CPU_FREQ_GOV_UBERDEMAND
-	set_second_phase_freq(1134000);
+	set_second_phase_freq(CONFIG_CPU_FREQ_GOV_UBERDEMAND_SECOND_PHASE_FREQ);
 #endif
 	msm8x60_init_tlmm();
 #ifdef CONFIG_BATTERY_SEC
