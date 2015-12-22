e69f56ba80a4bf60b96f6f5e8e33b78d26d3e968
bryan2894 <bryan2894@gmail.com>
2015-12-22 15:02:58 -0500

    Revert "selinux: workaround for samsung blobs"
    This reverts commit 0efe9884f00842067101f964479507ae57ec293e.

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3296,18 +3296,7 @@ static int selinux_file_mprotect(struct vm_area_struct *vma,
 			 * modified content.  This typically should only
 			 * occur for text relocations.
 			 */
-			 /* STARGO: Hack for Samsung blobs */
-			if (strcmp(current->comm, "ks") &&
-			    strcmp(current->comm, "mpdecision") &&
-			    strcmp(current->comm, "netmgrd") &&
-			    strcmp(current->comm, "qcks") &&
-			    strcmp(current->comm, "qmiproxy") &&
-			    strcmp(current->comm, "qmuxd") &&
-			    strcmp(current->comm, "rild") &&
-			    strcmp(current->comm, "rmt_storage") &&
-			    strcmp(current->comm, "sec-ril") &&
-			    strcmp(current->comm, "thermald"))
-				rc = file_has_perm(cred, vma->vm_file, FILE__EXECMOD);
+			rc = file_has_perm(cred, vma->vm_file, FILE__EXECMOD);
 		}
 		if (rc)
 			return rc;
