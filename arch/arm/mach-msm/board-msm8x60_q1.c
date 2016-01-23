137d7e36096e08bbb2aa31868054d12ae7b0f815
HomuHomu <ecoco666@gmail.com>
2016-01-23 11:41:57 +0900

    Revert "overclocking: Add Kconfig option"
    This reverts commit da13ad36cff5c7ba21ab8600bf8b08fdc17d3034.
    
    Change-Id: I1147aeea6186324977cf6fe04d5d64a2a12436b5

--- arch/arm/mach-msm/board-msm8x60_q1.c
+++ arch/arm/mach-msm/board-msm8x60_q1.c
@@ -790,11 +790,13 @@ static struct regulator_init_data saw_s0_init_data = {
 			.name = "8901_s0",
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
 			.min_uV = 800000,
+<<<<<<< HEAD
 #if defined(CONFIG_JPN_MODEL_SC_05D)
 			.max_uV = 1325000,
 #else
+=======
+>>>>>>> parent of da13ad3... overclocking: Add Kconfig option
 			.max_uV = 1250000,
-#endif
 		},
 		.consumer_supplies = vreg_consumers_8901_S0,
 		.num_consumer_supplies = ARRAY_SIZE(vreg_consumers_8901_S0),
@@ -8420,11 +8422,15 @@ static struct regulator_consumer_supply vreg_consumers_PM8901_S4_PC[] = {
 /* RPM early regulator constraints */
 static struct rpm_regulator_init_data rpm_regulator_early_init_data[] = {
 	/*	 ID       a_on pd ss min_uV   max_uV   init_ip    freq */
+<<<<<<< HEAD
 #if defined(CONFIG_JPN_MODEL_SC_05D)
 	RPM_SMPS(PM8058_S0, 0, 1, 1,  500000, 1325000, SMPS_HMIN, 1p60),
 #else
 	RPM_SMPS(PM8058_S0, 0, 1, 1,  500000, 1250000, SMPS_HMIN, 1p60),
 #endif
+=======
+	RPM_SMPS(PM8058_S0, 0, 1, 1,  500000, 1250000, SMPS_HMIN, 1p60),
+>>>>>>> parent of da13ad3... overclocking: Add Kconfig option
 	RPM_SMPS(PM8058_S1, 0, 1, 1,  500000, 1250000, SMPS_HMIN, 1p60),
 };
 
