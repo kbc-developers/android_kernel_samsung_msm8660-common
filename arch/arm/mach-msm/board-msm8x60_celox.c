07d2ef9ceafa093d4739f787b57a69b140ad76eb
bryan2894 <bryan2894@gmail.com>
2016-02-15 12:14:02 -0500

    cpufreq: cleanup govs that don't build
    Change-Id: Id412a36ea4f425623489234425111533a0e50092

--- arch/arm/mach-msm/board-msm8x60_celox.c
+++ arch/arm/mach-msm/board-msm8x60_celox.c
@@ -181,12 +181,6 @@
 #include <mach/tdmb_pdata.h>
 #endif
 
-#ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
-#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
-	int id_set_two_phase_freq(int cpufreq);
-#endif
-#endif 
-
 #ifdef CONFIG_CPU_FREQ_GOV_BADASS_2_PHASE
 	int set_two_phase_freq_badass(int cpufreq);
 #endif
@@ -16568,12 +16562,6 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 	/* CPU frequency control is not supported on simulated targets. */
 	if (!machine_is_msm8x60_rumi3() && !machine_is_msm8x60_sim())
 		acpuclk_init(&acpuclk_8x60_soc_data);
-
-#ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
-#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
-	id_set_two_phase_freq(1134000);
-#endif
-#endif 
 
 #ifdef CONFIG_CPU_FREQ_GOV_BADASS_2_PHASE
   	set_two_phase_freq_badass(CONFIG_CPU_FREQ_GOV_BADASS_2_PHASE_FREQ);
--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -389,42 +389,18 @@ extern struct cpufreq_governor cpufreq_gov_minmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG)
 extern struct cpufreq_governor cpufreq_gov_abyssplug;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_abyssplug)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLP)
-extern struct cpufreq_governor cpufreq_gov_slp;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_slp)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SUPERBAD)
-extern struct cpufreq_governor cpufreq_gov_superbad;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_superbad)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
-extern struct cpufreq_governor cpufreq_gov_lulzactive;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_lulzactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
-extern struct cpufreq_governor cpufreq_gov_pegasusq;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_pegasusq)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
-extern struct cpufreq_governor cpufreq_gov_lazy;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_lazy)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCARY)
 extern struct cpufreq_governor cpufreq_gov_scary;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_scary)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_VIRTUOUS)
 extern struct cpufreq_governor cpufreq_gov_virtuous;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_virtuous)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMOOTHASS)
-extern struct cpufreq_governor cpufreq_gov_smoothass;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_smoothass)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_wheatley)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
-extern struct cpufreq_governor cpufreq_gov_intellidemand;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_intellidemand)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR  	(&cpufreq_gov_interactivex)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_GALLIMAUFRY)
-extern struct cpufreq_governor cpufreq_gov_gallumaufry;
-#define CPUFREQ_DEFAULT_GOVERNOR  	(&cpufreq_gov_gallumaufry)
 #elif defined(CONFIG_CPU_FREQ_DEFAULG_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_lionheart)
@@ -443,9 +419,6 @@ extern struct cpufreq_governor cpufreq_gov_fantasy;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAKURACTIVE)
 extern struct cpufreq_governor cpufreq_gov_sakuractive;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_sakuractive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
-extern struct cpufreq_governor cpufreq_gov_ondemandx;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandx)
 #elif defined(CPU_FREQ_DEFAULT_GOV_BRAZILIANWAX)
 extern struct cpufreq_governor cpufreq_gov_brazilianwax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_brazilianwax)
