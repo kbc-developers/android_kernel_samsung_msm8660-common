be117c9d35e26ca55eba2d353e3281f84b9e50a4
Paul Keith <javelinanddart@gmail.com>
2016-02-21 06:19:54 -0800

    uberdemand: Enable cpufreq_notify_utilization
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- drivers/cpufreq/cpufreq_uberdemand.c
+++ drivers/cpufreq/cpufreq_uberdemand.c
@@ -649,7 +649,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	/* calculate the scaled load across CPU */
 	load_at_max_freq = (cur_load * policy->cur)/policy->cpuinfo.max_freq;
 
-//	cpufreq_notify_utilization(policy, load_at_max_freq);
+	cpufreq_notify_utilization(policy, load_at_max_freq);
 
 	/* Check for frequency increase */
 	if (max_load_freq > dbs_tuners_ins.up_threshold * policy->cur) {
